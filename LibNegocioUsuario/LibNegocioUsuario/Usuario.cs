54dd1e7fd808d1b2dc191a14ee543e85ab25a911
Lecter <leandro_valenzuela@hotmail.com>
2019-06-20 19:14:26 -0400

    probando

---
+++ LibNegocioUsuario/LibNegocioUsuario/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibNegocioUsuario
+{
+    public class Usuario
+    {
+        #region Atributos
+        string _mensaje;
+        bool _exito;
+        #endregion
+        
+    } // fin class
+
+} // fin namespace
